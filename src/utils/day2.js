fcc9277355aa9c93b15714084503d881adec6f76
Tudor Pavel <tudorv.pavel@gmail.com>
2017-12-04 22:38:26 +0200

    solve Day2-Bonus

--- src/utils/day2.js
+++ src/utils/day2.js
@@ -12,28 +12,42 @@ const add = (a, b) => {
   return a + b;
 }
 
-const rowChecksum = (row) => {
+const rowChecksumNormal = (row) => {
   return Math.max(...row) - Math.min(...row);
 }
 
-const finalChecksum = (rows) => {
-  return rows.map(rowChecksum).reduce(add, 0);
+const rowChecksumBonus = (row) => {
+  for (let i = 0; i < row.length - 1; i++) {
+    for (let j = i + 1; j < row.length; j++) {
+      if (row[i] % row[j] === 0) {
+        return row[i] / row[j];
+      } else if (row[j] % row[i] === 0) {
+        return row[j] / row[i];
+      }
+    }
+  }
+
+  return 0;
 }
 
-const day2 = (inputText) => {
+const finalChecksum = (rowChecksums) => {
+  return rowChecksums.reduce(add, 0);
+}
+
+const day2 = (inputText, rowChecksum) => {
   const rows = rowsFromInput(inputText);
 
   if (rows === null) {
     return 'invalid input';
   }
 
-  return finalChecksum(rows).toString();
+  return finalChecksum(rows.map(rowChecksum)).toString();
 }
 
 export const day2Normal = (inputText) => {
-  return day2(inputText);
+  return day2(inputText, rowChecksumNormal);
 };
 
 export const day2Bonus = (inputText) => {
-  return day2(inputText);
+  return day2(inputText, rowChecksumBonus);
 };
