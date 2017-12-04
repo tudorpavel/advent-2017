108091a634bbbb997c0405f7208d4eeb9fbb0ca9
Tudor Pavel <tudorv.pavel@gmail.com>
2017-12-04 21:24:38 +0200

    implementation for day1

--- src/utils/day1.js
+++ src/utils/day1.js
@@ -1,5 +1,21 @@
 const day1 = (inputText) => {
-  return inputText + inputText;
+  if (inputText.match(/^(\d+)$/) == null) {
+    return 'invalid input';
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < inputText.length - 1; i++) {
+    if (inputText[i] === inputText[i + 1]) {
+      result += parseInt(inputText[i], 10);
+    }
+  }
+
+  if (inputText[inputText.length - 1] === inputText[0]) {
+    result += parseInt(inputText[0], 10);
+  }
+
+  return result.toString();
 };
 
 export default day1;
